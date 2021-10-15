14d45baa3533ae97ea12739d362c85ddf63ca337
khurd21 <kyle.hurd@wsu.edu>
2021-10-15 09:24:01 -0700

    Creating the template for project
    Added app.py and config.py to initialize the application. Also created app folder which will host the Controller, Model, and View resources.

---
+++ app.py
@@ -0,0 +1,24 @@
+
+
+
+from app import create_app, db
+
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db} # Post?
+
+
+@app.before_first_request
+def init_db(*args, **kwargs):
+    db.create_all()
+    return
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from flask_login import LoginManager
+
+from config import Config
+
+db = SQLAlchemy()
+moment = Moment()
+login = LoginManager()
+login.login_view = 'auth.login'
+
+
+def create_app(config_class=Config):
+
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    app.static_folder = config_class.STATIC_FOLDER
+    app.template_folder = config_class.TEMPLATE_FOLDER
+    
+    db.init_app(app=app)
+    moment.init_app(app=app)
+    login.init_app(app=app)
+
+    # Blueprints here:
+
+    return app
+
+
