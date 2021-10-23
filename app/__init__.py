0e6b0669d40ce540e081f615986aeb41f89e78ff
khurd21 <kyle.hurd@wsu.edu>
2021-10-23 13:33:42 -0700

    Added Auth Forms and prepped for Bootstrap
    Added a Bootstrap object to the app/__init__.py and added dependencies to requirements.txt.
    
    Added RegisterForm and LoginForm in Controller/auth_forms.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
 from flask_login import LoginManager
@@ -9,6 +10,7 @@ from config import Config
 
 db = SQLAlchemy()
 moment = Moment()
+bootstrap = Bootstrap()
 login = LoginManager()
 login.login_view = 'auth.login'
 
@@ -22,6 +24,7 @@ def create_app(config_class=Config):
     
     db.init_app(app=app)
     moment.init_app(app=app)
+    bootstrap.init_app(app=app)
     login.init_app(app=app)
 
     return app
